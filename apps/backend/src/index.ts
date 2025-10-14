dc6b4515640a1077c44c809cc31d9ac3c45ee381
Bamose <anellanegash@gmail.com>
2025-10-14 10:09:05 +0300

    fix: server access

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -9,7 +9,7 @@ import { logger } from "@song-app/utils";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -78,7 +78,7 @@ app.get("/docs", (_req, res) => {
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       logger.log(`Backend server running on http://localhost:${PORT}`);
     });
   })
