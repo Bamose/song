46b879c4bcd16f137adb2cb6b34cedee6b031332
Bamose <anellanegash@gmail.com>
2025-10-14 10:32:10 +0300

    fix: cors

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://song-api-sk0f.onrender.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/songs", songRoutes);
